67b69f0ee823a367f97aa57fea78a2ec533c9d97
mojw2 <mojw2@192.168.1.95>
2022-08-03 19:07:56 -0400

    pushing

--- src/team/g3/DemoG3.java
+++ src/team/g3/DemoG3.java
@@ -3,6 +3,9 @@ package team.g3;
 public class DemoG3 {
 	
 	public static void main(String[] args) {
+		
+		 
+		System.out.println("Commit");
 		System.out.println("Test");
 		
 		System.out.println("Yousuf Babarzai");
