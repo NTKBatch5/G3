51a1fedf42fd48fc1611271b16afc42c43f51c1b
Jacobo <Jacobo@LAPTOP-6RNGU2IQ>
2022-08-04 08:43:36 -0400

    Script from Jacobo

--- src/team/g3/DemoG3.java
+++ src/team/g3/DemoG3.java
@@ -5,6 +5,7 @@ public class DemoG3 {
 	public static void main(String[] args) {
 		
 	System.out.println("Automation script");
+	System.out.println ("Automation script from Jacob");
 	
 		System.out.println("Commit");
 		//this was added
